1de6e83f86878b6dd08eb7b920692bcf8238c2df
newnakashima <newnakashima2014@gmail.com>
2017-03-28 21:59:47 +0900

    webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 var path = require('path');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  // filename: "[name].[contenthash].css"
+  filename: "style.css"
+  // ,disable: process.env.NODE_ENV === "development"
+});
+
 module.exports = {
   entry: './webpack_build/main.js',
   output: {
@@ -7,11 +15,25 @@ module.exports = {
     path: path.resolve(__dirname, 'out/dist')
   },
   module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+        use: [{
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader',
+          options: {
+          // includePaths: ["node_modules"]
+          }
+        }],
+        fallback: "style-loader"
+      })
+    }]
+  },
+  plugins: [
+    extractSass
+  ],
+  devServer: {
+    port: 3000
   }
 };
