8d8e6a946feef9c4c2058c944fc94fec38978204
newnakashima <newnakashima2014@gmail.com>
2017-03-31 13:31:04 +0900

    twitterボタンが設置できない

--- webpack_build/main.js
+++ webpack_build/main.js
@@ -38,6 +38,7 @@ function initMap(location) {
     <div class="field">
       <p class="control">
         <button class="button is-primary" type="button" name="cast-feeling" id="cast-feeling">cast</button>
+<a href="https://twitter.com/share" class="twitter-share-button" data-via="newnakashima" data-size="large" data-hashtags="test">Tweet</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
       </p>
     </div>
     <div class="content is-medium" id="cast-output">
